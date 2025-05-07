6184c350f23f97d304b2d0a60082bd504eb3501d
Kael-The-Vegetable <kkostiuk3@nait.ca>
2025-05-07 14:07:22 -0600

    ✅ Adding a test to the TickManager
    Checking if ticks work appropriately

--- Assets/Scripts/Managers/TickManager.cs
+++ Assets/Scripts/Managers/TickManager.cs
@@ -1,9 +1,16 @@
 using BasicUtilities;
+using System;
 using UnityEngine;
 public class TickManager : Singleton<TickManager>
 {
+	[field: SerializeField] public int TicksPerSecond { get; set; } = 60;
+
+	public static event Action OnTick;
 	protected override void Initialize()
 	{
-
+		float tickTime = 1f / TicksPerSecond;
+		OnTick += () => OneShotTimer.Delay(tickTime, OnTick, destroyCancellationToken);
+		OnTick += () => Debug.Log(Time.time);
+		OnTick.Invoke();
 	}
 }
